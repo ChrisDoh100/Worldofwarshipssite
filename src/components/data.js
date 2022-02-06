1fd9e5a39ce9eeed49dfbd7d56a02dae67e7d2bb
ChrisDoh100 <christopherdoherty14@gmail.com>
2022-02-06 20:48:43 +0000

    Fixed Sidebar and scrolling issue, made sidebar responsive also, collapses when page width decreases

--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { DataFilter } from "../datafilter/helperfunctions";
 import './data.css'
@@ -8,16 +9,20 @@ import Header from "./header";
 
 const DataDisplay=()=>{
     const  {state}  = useLocation();
-    console.log("state",{state})
+    const [truepadding,setTruePadding] = useState(220);
+    //console.log("state",{state})
     let data = {}
     data=state;
     let name = state.name;
-    console.log(name);
+    //console.log(name);
     let output = data;
-    console.log(output)
+    //console.log(output)
     const entries = output.data
     let whateverthefuckthisis=DataFilter(entries)
-    console.log(whateverthefuckthisis)
+    //console.log(whateverthefuckthisis)
+    window.addEventListener("scroll",()=>{
+        setTruePadding(218-window.scrollY);
+    });
     
     return(
         <>
@@ -25,19 +30,17 @@ const DataDisplay=()=>{
             <div className="nameanimation">
                 <h1 className="name">{name}</h1>
             </div>
-            <div className="main">
-                <div className="leftmenu">
-                    <h2 className="leftmenuheader">Data</h2>
-                    {Object.keys(whateverthefuckthisis).map(value=><p key={value}>{value}</p>)}
+            <div className="leftmenu" id="sidenav">
+                <div id="leftmenuinner" className="leftmenuinner" style={{"paddingTop":truepadding+"px"}}>
+                    <div className="leftmenuinnerinner">
+                        <h2 className="leftmenuheader">Data</h2>
+                        {Object.keys(whateverthefuckthisis).map(value=><p key={value}>{value}</p>)}
+                    </div>
                 </div>
-                <div className="playerdata">
-                    <h1 className="titlecontent">Here is the title of the Statistic</h1>
-                    <p className="pcontent">here we begin to discribe the data and the story thhcjakdhaskjhdjashdjkhasjkdhaskjhdkahdkhaskdhe describes the actual statistics.</p>
-                </div>
-                <div className="rightmostcontent">
-                    some random shit
-
-                </div>
+            </div>
+            <div className="playerdata">
+                <h1 className="titlecontent">Here is the title of the Statistic</h1>
+                <p className="pcontent">{Object.keys(whateverthefuckthisis).map(value=><p key={value}>{value}</p>)}</p>
             </div>
         </>
     )
