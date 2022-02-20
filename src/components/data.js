834fbbe7f934a59e9983f5e233c5e4e5c5c90be6
ChrisDoh100 <christopherdoherty14@gmail.com>
2022-02-20 18:36:01 +0000

    Integrated content and header functionality from sidebar

--- src/components/data.js
+++ src/components/data.js
@@ -12,14 +12,15 @@ import Header from "./header";
 const DataDisplay= ()=>{
     const dispatch = useDispatch()
     const story = useSelector(state=>state);
-
+    const [title,setTitle]=useState(" ");
+    const [titlevalue,setTitlevalue] = useState(0);
+    const [truepadding,setTruePadding] = useState(220);
     useEffect(async ()=>{
         const stories = await getStory();
         dispatch(setStory(stories))
     },[dispatch])
     const cleanstory=story[1];
     const  {state}  = useLocation();
-    const [truepadding,setTruePadding] = useState(220);
     let data = {}
     data=state;
     let name = state.name;
@@ -32,6 +33,12 @@ const DataDisplay= ()=>{
         }
         console.log(truepadding);
     });
+    const Clicked=(somevalue)=>{
+        console.log({somevalue})
+        setTitle(somevalue);
+        setTitlevalue(whateverthefuckthisis[somevalue])
+        console.log("You're a dumfuck")
+    }
     return(
         <>
         <Header/>
@@ -42,12 +49,12 @@ const DataDisplay= ()=>{
                 <div id="leftmenuinner" className="leftmenuinner" style={{"paddingTop":truepadding+"px"}}>
                     <div className="leftmenuinnerinner">
                         <h2 className="leftmenuheader">Data</h2>
-                        {Object.keys(whateverthefuckthisis).map(value=><p key={value}>{value}</p>)}
+                        {Object.keys(whateverthefuckthisis).map(value=><p onClick={()=>Clicked(value)} key={value} >{value}</p>)}
                     </div>
                 </div>
             </div>
             <div className="playerdata">
-                <h1 className="titlecontent">Here is the title of the Statistic</h1>
+                <h1 className="titlecontent">{title}{titlevalue}</h1>
                 <p className="pcontent">{cleanstory}</p>
             </div>
         </>
