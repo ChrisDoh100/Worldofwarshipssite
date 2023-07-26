f1a208753dd7fd697fdb7584f0b9475b440fa555
Niall McCarter <christopherdoherty14@gmail.com>
2023-07-26 17:54:13 +0100

    added graph fullfuctionality

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,13 +1,19 @@
 import { Table } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './DataTable.css';
 import { SaveUserData } from "../services/dataService";
 import DisplayChart from "./ChartDisplay";
+import { Line } from "react-chartjs-2";
+import { GetUserData } from "../services/dataService";
 
 const TableDisplay = ({TableData,Currentuser})=>{
     let logged = useSelector(state=>state.LoggedIn.loggedIn)
     let Displayname = useSelector(state=>state.DisplayUsername.name);
+    const [graphlabels,setgraphlabels] =useState([])
+    const [graphdata,setgraphData] = useState([]) 
+
+
     console.log("Currentname",Currentuser);
     console.log("Displayusername",Displayname);
     //this will save the user data at this point in time for that specific time.
@@ -16,8 +22,16 @@ const TableDisplay = ({TableData,Currentuser})=>{
         console.log({Saved});
 
     }
+    //now we populate the map with the given possible data values in the backend when the graph button is clicked.
+    const  getData = async(value)=>{
+        let res = await GetUserData({Currentuser,value});
+        console.log({res})
+        setgraphlabels(res.map(x=>x.createdAt))
+        setgraphData(res.map(x=>x.value))
+    }
     //this will be used to get individual stat data based on the type of graphed selected.
     let length = (logged && (Displayname==Currentuser))?1:4;
+    //<Line data={{labels:valueData[identity]?.map(x=>x.createdAt),datasets: [{label: identity, data:valueData[identity]?.map(x=>console.log("Wtf",x.value))}]}}/>
     return (
         <>
         <Table striped bordered hover variant="dark" className="table" style={{marginBottom: 0+'px'}} >
@@ -52,12 +66,12 @@ const TableDisplay = ({TableData,Currentuser})=>{
                                     {key[1]}
                                 </td>
                                 {(logged && (Displayname==Currentuser))?<td><button className="btn btn-dark" onClick={()=>Savevalue(key)}>Click to Save</button></td>:null} 
-                                {(logged && (Displayname==Currentuser))?<td><button className="btn btn-dark" type="button" data-bs-target={`#${identity}`} data-bs-toggle="collapse" aria-controls={`${identity}`} aria-expanded="false">Graph Data</button></td>:null}
+                                {(logged && (Displayname==Currentuser))?<td><button className="btn btn-dark" type="button" data-bs-target={`#${identity}`} onClick={()=>getData(identity)} data-bs-toggle="collapse" aria-controls={`${identity}`} aria-expanded="false">Graph Data</button></td>:null}
                                 
                             </tr>
                             <tr className="collapse out" id = {`${identity}`} colSpan="6">
                                 <td colSpan={6}>
-                                    {(logged && (Displayname==Currentuser))?<DisplayChart data={[Currentuser,key[0]]}/>:null}
+                                    {(logged && (Displayname==Currentuser))?<Line data={{labels:graphlabels,datasets: [{label: identity, data:graphdata}]}}/>:null}
                                 </td>
                             </tr>
                         </>
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -15,7 +15,7 @@ export const GetUserData = async(data)=>{
     console.log("dddata",data);
     const currentuser = data[0];
     const currentvalue = data[1];
-    const response = await axios.post(`${baseUrl}/api/infos/userdata`,{username:currentuser,value:currentvalue},{ headers: {"Authorization" : `Bearer ${localStorage.token}`}});
-    console.log(response);
+    const response = await axios.post(`${baseUrl}/api/infos/userdata`,{username:data[0],value:data[1]},{ headers: {"Authorization" : `Bearer ${localStorage.token}`}});
+    console.log({response});
     return response.data;
 }
